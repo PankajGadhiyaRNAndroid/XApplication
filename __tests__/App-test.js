33978120858d85c43bfcdf21cc93f8ba580a4ce0
Pankaj <info.pankajgadhiya@gmail.com>
2019-11-17 12:30:20 +0530

    jest path change

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -5,10 +5,11 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import XebiaApplication from '../src/components/Mainroutes/XebiaApplication';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  renderer.create(<XebiaApplication />);
 });
